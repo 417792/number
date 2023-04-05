6ea0e273498248c83a1b9bbb56905b95e14e931b
417792 <115142684+417792@users.noreply.github.com>
2023-04-05 20:51:17 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,76 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    basic.pause(1000)
-    radio.sendNumber(_)
-    basic.showString("" + (_))
-})
 input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(_)
+    group += 1
 })
-let _ = 0
-_ = 0
+input.onButtonPressed(Button.AB, function () {
+    if (group != 99) {
+        group += 1
+        basic.showNumber(group)
+        basic.showString(code)
+        radio.setGroup(group)
+        radio.sendValue(code, group)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.setGroup(group)
+    basic.showNumber(group)
+    if (group == 1) {
+        code = "?"
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+        `)
+})
+radio.onReceivedValue(function (name, value) {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showNumber(value)
+    basic.showString(name)
+    code = name
+    group = value
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+let code = ""
+let group = 0
+radio.setGroup(99)
+group = 0
+code = "You can do it!(maybe......)"
